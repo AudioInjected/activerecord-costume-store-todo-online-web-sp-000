5f5aabad43201d33816f959886003522e38bc720
Rafael Batista <rafaelb24@yahoo.com>
2019-01-04 19:31:15 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change 
     create_table :haunted_houses do |t|
       t.string :name 
-      t.text :location
+      t.string :location
     end
   end 
 end 
