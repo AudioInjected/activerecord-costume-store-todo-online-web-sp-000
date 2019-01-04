f72b5f3b8c0565f53b1d6dc0185244c00625c0fc
Rafael Batista <rafaelb24@yahoo.com>
2019-01-04 18:48:24 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.string "image_url"
+    t.text "image_url"
   end
 
 end
