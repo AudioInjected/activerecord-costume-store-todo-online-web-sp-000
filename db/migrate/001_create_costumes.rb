f1bae4325a2018ba54d75cb574932b90f9394596
Rafael Batista <rafaelb24@yahoo.com>
2019-01-04 18:43:58 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,5 +9,10 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[4.2] 
-  
+  def change 
+    create_table costumes do |t|
+      t.string :name 
+      
+    end
+  end 
 end 
