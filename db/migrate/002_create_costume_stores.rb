e9a2e5e3be129328c39e6df38aeb03c34c0c251b
Rafael Batista <rafaelb24@yahoo.com>
2019-01-04 18:54:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2] 
   
   def change 
-    create_table :costume_stores |t|
+    create_table :costume_stores do |t|
       t.string :name 
     end
   end 
